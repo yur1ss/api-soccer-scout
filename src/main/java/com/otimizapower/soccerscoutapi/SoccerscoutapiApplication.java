c4b5697e6349215433bf4671b6a600d1db752d27
yur1ss <ssoares.yuri@gmail.com>
2022-04-07 15:07:17 -0300

    primeiro commit

---
+++ src/main/java/com/otimizapower/soccerscoutapi/SoccerscoutapiApplication.java
@@ -0,0 +1,13 @@
+package com.otimizapower.soccerscoutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerscoutapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerscoutapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otimizapower/soccerscoutapi/SoccerscoutapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otimizapower.soccerscoutapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerscoutapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
